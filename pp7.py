e2776d50c787c0eb129ce9c2ae717735718d380f
Emily Ferretti <ferrettiek@slu.edu>
2016-04-28 11:39:14 -0500

    step 4 build, findReplacements() function correct

--- pp7.py
+++ pp7.py
@@ -18,9 +18,8 @@ def readFile():
   wordset=set()
   while line:
     line=line.strip()
-    wordset.add(line)
+    wordset.add(line) #add to the set of words
     line=wordsfile.readline()
- 
   return wordset
 
 def validateWord(word,correctSet):
@@ -30,15 +29,16 @@ def validateWord(word,correctSet):
         entry to the dictionary if the misspelled word and the replacement word have 4 letters that
         are the same in the same position.
      3. if the two words are not equal in length but are both longer than 3 characters and 
-        the first three letters of the words are the same, add an entry to the dictionary"""
+        the first three letters of the words are the same, add an entry to the dictionary
+     Entries are indexed by a integer"""
   replacements={} #create a dictionary for replacement words
   entry=0
   if word in correctSet:
     return replacements
   elif word not in correctSet:
-    if len(word)<4:
+    if len(word)<4:  #rule 1
       return replacements
-    elif len(word)>=4:
+    elif len(word)>=4: #rule 2 and 3
       for w in correctSet:
         if len(word)==len(w):
           counter=0
@@ -54,6 +54,37 @@ def validateWord(word,correctSet):
             replacements[entry]=w
   return replacements
 
+def findReplacement(replacementWords,wrongWord):
+  """receives and displays the dictionary containing possible replacement words
+     and the misspelled word. 
+      User must enter the integer number representing the replacement word they desire
+      OR a -1 to keep the misspelled word with no change.
+       Corresponding dictionary entry is returned if integer is entered.
+       Misspelled word is returned if -1 is entered."""
+  print '\nPossibly misspelled word:',wrongWord
+  print '\nReplacement Options:\n'
+
+  for keys,values in replacementWords.items(): #print all the replacement options
+    print 'Option',str(keys),':\t'+values
+
+  userWordChoice=''
+
+  replacekey=None
+  while str(replacekey)!='-1' and replacekey not in replacementWords.keys():
+    try:
+      print '\nCurrently Checking Spelling of:',wrongWord
+      replacekey=int(raw_input('\nEnter which replacement option you want (-1 for no change):\n'))
+      if str(replacekey)=='-1':
+        userWordChoice= wrongWord
+      elif int(replacekey) in replacementWords:
+        userWordChoice=replacementWords[replacekey]
+      elif int(replacekey) not in replacementWords:
+        print 'Your input must be an option or -1'
+    except ValueError:
+      print str(replacekey),'is not a valid key or -1'
+    except EOFError:
+      print 'Please enter an integer of the replacement word or -1 to keep it the same'
+  return userWordChoice
 
 #main body of program
 #receive the name of file to check and make output file name
@@ -66,22 +97,32 @@ while not infile:
   except IOError:
     print 'sorry. unable to open file', infilename
 
-
+#create the filename and file for the checked data
 outfilename=infilename.strip('.txt')
 outfilename=outfilename+'Chk.txt'
 outfile=file(outfilename,'w')
 
-
+#create the set of correct words
 setofCorrectWords=readFile()
 
+#read each line of the file
 line=infile.readline()
 while line:
-  words=line.split()
-  for w in words:
+  words=line.split() #split up the words in the line
+  for w in words: #check every word
     correct=validateWord(w,setofCorrectWords)
-  line=infile.readline()
+    replacementWord=w
+    if len(correct)!=0:
+      replacementWord=findReplacement(correct,w)
+    outfile.write(replacementWord) #write the word after checking and making any changes
+    outfile.write(' ') #maintain spacing
+  line=infile.readline() 
+  outfile.write('\n') #maintain line breaks
 
-aword='scientst'
-replacementDict=validateWord(aword,setofCorrectWords)
-for keys,values in replacementDict.items():
-  print 'option',str(keys),values
+#close all files
+outfile.close()
+infile.close()
+
+#allow the user to end the program when ready
+print
+raw_input('Press ENTER to close')
